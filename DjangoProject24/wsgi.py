9ac545916b74a84bff90e120d5915debdef49ec5
bardianozari <bardia.nozari1391@gmail.com>
2026-06-27 23:10:14 +0330

    fix: update project name in wsgi and management files2

--- DjangoProject24/wsgi.py
+++ DjangoProject24/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_romantic_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject24.settings')
 
 application = get_wsgi_application()
